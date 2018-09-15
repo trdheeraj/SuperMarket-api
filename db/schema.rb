7644ca29430a0d9fff4b788bf25ecc9d6a3be8de
Dheeraj Thasma Ravindranath <dheerajthasmaravindranath@Mac-Dev-Dheeraj.local>
2018-09-15 21:31:07 +0530

    Adding product table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_15_154156) do
+ActiveRecord::Schema.define(version: 2018_09_15_154737) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "address"
@@ -27,6 +27,16 @@ ActiveRecord::Schema.define(version: 2018_09_15_154156) do
     t.integer "address_id"
     t.integer "mobile"
     t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "quantity"
+    t.float "rate"
+    t.string "hsn"
+    t.string "gst"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
