4f1c025f03f7ec1d74452c35e25d95ff6bc0e34f
Dheeraj Thasma Ravindranath <dheerajthasmaravindranath@Mac-Dev-Dheeraj.local>
2018-09-17 22:20:09 +0530

    Adding show method for customer

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,8 +1,19 @@
 module Api::V1
 	class CustomersController < ApplicationController
+		before_action :set_customer, only: [:show]
+		
 		def index
 	      @customers = Customer.all
 	      render json: @customers
+	    end
+
+	    def show
+	    	render json: @customer
+	    end
+
+	    private
+	    def set_customer
+	    	@customer = Customer.find(params[:id])
 	    end
 	end
 end
