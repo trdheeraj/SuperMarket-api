5f890fd143b378f95ce8556b01ea03d971257395
tdheeraj94 <dheeraj.tr@weinvest.net>
2018-09-23 22:59:41 +0530

    Customer creation

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,11 +1,19 @@
 module Api::V1
 	class CustomersController < ApplicationController
 		before_action :set_customer, only: [:show]
-		
+
 		def index
 	      @customers = Customer.all
 	      render json: @customers
 	    end
+
+	    def create	      
+		  @customer = Customer.new(customer_params)
+		  @address = Address.create(address_params)
+		  @customer.address = @address
+		  @customer.save!
+		  render json: @customer
+		end
 
 	    def show
 	    	render json: @customer
@@ -15,5 +23,13 @@ module Api::V1
 	    def set_customer
 	    	@customer = Customer.find(params[:id])
 	    end
+
+	    def customer_params
+			params.require(:customer).permit(:name, :mobile, :email)
+		end
+
+		def address_params
+			params.require(:address).permit(Address.column_names)
+		end
 	end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,6 @@
 class Customer < ApplicationRecord
 	belongs_to :address, dependent: :destroy
+
 	has_many :sales_invoices
 	has_many :customer_transactions
 
