2d8e097a57c42812b6e0386ba2b0d27d58b0044b
tdheeraj94 <dheeraj.tr@weinvest.net>
2018-10-13 23:07:59 +0530

    Idendation correction

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,35 +1,35 @@
 module Api::V1
-	class CustomersController < ApplicationController
-		before_action :set_customer, only: [:show]
+  class CustomersController < ApplicationController
+    before_action :set_customer, only: [:show]
 
-		def index
-	      @customers = Customer.all
-	      render json: @customers
-	    end
+    def index
+      @customers = Customer.all
+      render json: @customers
+    end
 
-	    def create	      
-		  @customer = Customer.new(customer_params)
-		  @address = Address.create(address_params)
-		  @customer.address = @address
-		  @customer.save!
-		  render json: @customer
-		end
+    def create
+      @customer = Customer.new(customer_params)
+      @address = Address.create(address_params)
+      @customer.address = @address
+      @customer.save!
+      render json: @customer
+    end
 
-	    def show
-	    	render json: @customer
-	    end
+    def show
+      render json: @customer
+    end
 
-	    private
-	    def set_customer
-	    	@customer = Customer.find(params[:id])
-	    end
+    private
+    def set_customer
+      @customer = Customer.find(params[:id])
+    end
 
-	    def customer_params
-			params.require(:customer).permit(:name, :mobile, :email)
-		end
+    def customer_params
+      params.require(:customer).permit(:name, :mobile, :email)
+    end
 
-		def address_params
-			params.require(:address).permit(Address.column_names)
-		end
-	end
+    def address_params
+      params.require(:address).permit(Address.column_names)
+    end
+  end
 end
